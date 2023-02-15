393328378b26246f2cd9cec9b17fe8caeb22ac9b
natehahn <nate.hahn@comcast.net>
2023-02-14 22:02:25 -0700

    Added proper jpg

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -4,4 +4,4 @@ mech_cars <- read.csv('MechaCar_mpg.csv',stringsAsFactors = F) #read in dataset
 
 lm(vehicle_length ~ vehicle_weight + spoiler_angle + ground_clearance + AWD + mpg,data=mech_cars)
 
-summary(lm(vehicle_length ~ vehicle_weight + spoiler_angle + ground_clearance + AWD + mpg,data=mech_cars))
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mech_cars))
