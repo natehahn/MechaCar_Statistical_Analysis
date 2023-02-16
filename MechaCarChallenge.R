d763dede29e3c635a1dd2803780d82a1c85d623d
natehahn <nate.hahn@comcast.net>
2023-02-15 21:28:37 -0700

    Added final R code

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,8 +1,9 @@
 library(dplyr)
+library(tidyverse)
 
 mech_cars <- read.csv('MechaCar_mpg.csv',stringsAsFactors = F) #read in dataset
 
-lm(vehicle_length ~ vehicle_weight + spoiler_angle + ground_clearance + AWD + mpg,data=mech_cars)
+linear <- lm(vehicle_length ~ vehicle_weight + spoiler_angle + ground_clearance + AWD + mpg,data=mech_cars)
 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mech_cars))
 
@@ -21,7 +22,9 @@ t.test(x = suspension_coils$PSI, mu=1500)
 t.test(formula = PSI, data = suspension_coils, subset = Manufacturing_Lot == "Lot1")
 
 lot1 <- filter(suspension_coils,Manufacturing_Lot == "Lot1")
+lot2 <- filter(suspension_coils,Manufacturing_Lot == "Lot2")
+lot3 <- filter(suspension_coils,Manufacturing_Lot == "Lot3")
 
-t.test(x = suspension_coils$PSI, subset=lot1)
-t.test(suspension_coils$PSI, subset=Lot2)
-t.test(suspension_coils$PSI, subset=Lot3)
+t.test(lot1$PSI,mu=1500)
+t.test(lot2$PSI, mu=1500)
+t.test(lot3$PSI, mu=1500)
